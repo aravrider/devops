0b56e652cf3a02826ea2a06dcc6cc471d2353d9c
aman <aravrider3@gmail.com>
2025-01-04 23:02:51 +0530

    poll trigger2

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/info")
 def lw():
-    return "Welcome to0000000000000000000 LW...."
+    return "Welcome to00000000000000000009999 LW...."
 
 
 app.run(host='0.0.0.0')
